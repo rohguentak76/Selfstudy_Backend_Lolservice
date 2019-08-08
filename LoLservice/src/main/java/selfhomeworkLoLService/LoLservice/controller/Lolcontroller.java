9432f0ff13984e67e377ce70131f123cdd55b7a4
rohguentak <rhgt78@gmail.com>
2019-08-08 18:28:43 +0900

    client 단에서 summoner이름으로 encryed된 id를 받아서 leagueposition을 받아오는 controller추가

--- LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/controller/Lolcontroller.java
+++ LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/controller/Lolcontroller.java
@@ -1,19 +1,28 @@
 package selfhomeworkLoLService.LoLservice.controller;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import selfhomeworkLoLService.LoLservice.domain.Encrypedinfo;
 import selfhomeworkLoLService.LoLservice.domain.Summonerdomain;
 import selfhomeworkLoLService.LoLservice.service.Lolservice;
+
+import java.util.List;
 
 @RestController
 public class Lolcontroller {
     @Autowired
     Lolservice lolservice;
 
+
     @GetMapping("/selfhomeworkLoLService/LoLService/by-summoner-name/{summonerName}")
-    public Summonerdomain getsummonerdomainbyname(@PathVariable String summonerName){
+    public Summonerdomain getsummonerdomainbyname(@PathVariable String summonerName) {
         return lolservice.getSummonerdomain(summonerName);
+    }
+    @GetMapping("/selfhomeworkLoLService/LoLService/by-encryped-summonername/{summonerName}")
+    public List<Encrypedinfo> getleaguepositionbyEncrypedname(@PathVariable String summonerName){
+        return lolservice.getLeaguepositionByencrypedsummonerName(summonerName);
     }
 }
