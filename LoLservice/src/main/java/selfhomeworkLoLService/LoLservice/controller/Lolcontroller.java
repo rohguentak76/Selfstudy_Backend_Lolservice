a0451c3418c962e4bb2d1fb233a795d599eabeef
rohguentak <rhgt78@gmail.com>
2019-08-08 16:33:25 +0900

    add service class and controller class

---
+++ LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/controller/Lolcontroller.java
@@ -0,0 +1,7 @@
+package selfhomeworkLoLService.LoLservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Lolcontroller {
+}
---
+++ LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/service/Lolservice.java
@@ -0,0 +1,8 @@
+package selfhomeworkLoLService.LoLservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Lolservice {
+
+}
