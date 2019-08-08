ffc24e8cdefb52ec1714778260be63e851e2ac66
rohguentak <rhgt78@gmail.com>
2019-08-08 18:25:46 +0900

    받아온 encrypedsummonerid를 통해 leagueposition을 api를통해 받아올수 잇도록 class추가

--- LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/service/Lolservice.java
+++ LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/service/Lolservice.java
@@ -1,26 +1,25 @@
 package selfhomeworkLoLService.LoLservice.service;
 
-<<<<<<< HEAD
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import selfhomeworkLoLService.LoLservice.api.LolserviceapiClient;
-=======
-import org.springframework.stereotype.Service;
->>>>>>> 8f911474029289f9545d3d6a1b4df2ac99f993f0
+import selfhomeworkLoLService.LoLservice.domain.Encrypedinfo;
 import selfhomeworkLoLService.LoLservice.domain.Summonerdomain;
+
+import java.util.List;
 
 @Service
 public class Lolservice {
-<<<<<<< HEAD
     @Autowired
     LolserviceapiClient lolserviceapiClient;
 
     public Summonerdomain getSummonerdomain(String summonerName) {
         return lolserviceapiClient.requestSummonerdomain(summonerName);
-=======
-
-    public static Summonerdomain getSummonerdomain(String summonerName) {
-        return LolserviceapiClient.requestSummonerdomain(summonerName);
->>>>>>> 8f911474029289f9545d3d6a1b4df2ac99f993f0
+    }
+    public List<Encrypedinfo> getLeaguepositionByencrypedsummonerName(String summonerName){
+        String encrypedId = getSummonerdomain(summonerName).getId();
+        List<Encrypedinfo> encrypedinfoList = lolserviceapiClient.requestleagueposition(encrypedId);
+        return encrypedinfoList;
     }
 }
