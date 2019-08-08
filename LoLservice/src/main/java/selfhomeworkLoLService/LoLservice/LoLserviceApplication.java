c32812e7c26a2b44e10cb12a518735e6fabb6813
rohguentak <rhgt78@gmail.com>
2019-08-08 15:43:49 +0900

    add initial code

---
+++ LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/LoLserviceApplication.java
@@ -0,0 +1,13 @@
+package selfhomeworkLoLService.LoLservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoLserviceApplication.class, args);
+	}
+
+}
