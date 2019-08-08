931b6baa874157f3f14a2940fa7766a39144544e
rohguentak <rhgt78@gmail.com>
2019-08-08 18:37:13 +0900

    get rid of conflict

--- LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/api/LolserviceapiClient.java
+++ LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/api/LolserviceapiClient.java
@@ -8,21 +8,12 @@ import selfhomeworkLoLService.LoLservice.domain.Summonerdomain;
 
 @Service
 public class LolserviceapiClient {
-<<<<<<< HEAD
     private final String appid = "RGAPI-782a0a2c-ce4d-44a9-9a8d-0cdebc38751b";
     private final String summonerUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={appid}";
 
     @Autowired
     private RestTemplate restTemplate;
     public Summonerdomain requestSummonerdomain(String summonerName) {
-=======
-    private String appid = "RGAPI-782a0a2c-ce4d-44a9-9a8d-0cdebc38751b";
-    private String summonerUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={appid}";
-
-    @Autowired
-    RestTemplate restTemplate;
-    public static Summonerdomain requestSummonerdomain(String summonerName) {
->>>>>>> 8f911474029289f9545d3d6a1b4df2ac99f993f0
         return restTemplate.exchange(summonerUrl, HttpMethod.GET,null,Summonerdomain.class,summonerName,appid).getBody();
     }
 }
