3b5e0fa410392257adfc9bc967951125488eaa24
rohguentak <rhgt78@gmail.com>
2019-08-08 16:40:58 +0900

    add domain added by summonername

---
+++ LoLservice/src/main/java/selfhomeworkLoLService/LoLservice/domain/Summonerdomain.java
@@ -0,0 +1,11 @@
+package selfhomeworkLoLService.LoLservice.domain;
+
+public class Summonerdomain {
+    public int profileIconId;
+    public String name;
+    public String puuid;
+    public long summonerLevel;
+    public long revisionDate;
+    public String id;
+    public String accountId;
+}
